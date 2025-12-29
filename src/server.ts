ad894af07674659b1f50b3872a6debd3779c943c
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2025-12-29 16:41:13 -0300

    feat: zod e validacoes

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,33 @@ import fastify from "fastify"
 // import knex from "knex"
 import crypto from 'node:crypto'
 import { db } from "./database.js"
+import { env } from "../env/index.js"
 
+let knex = db
 
 const app = fastify()
 
 app.get('/hello', async () => {
+    const transactions = await knex('transactions')
+    .where('amount', 100)
+    .select('*')
 
- const transactions =  await db('transactions').insert({
-  id: crypto.randomUUID(),
-  title: 'transacao de teste',
-  amount: 1000,
-  created_at: new Date()
-})
+
+
+
+//  const transactions =  await db('transactions').insert({
+//   id: crypto.randomUUID(),
+//   title: 'transacao de teste',
+//   amount: 1000,
+//   created_at: new Date()
+// }).returning('*')
 console.log('deu certo')
     return transactions
     
 })
 
 app.listen({
-        port: 3333,
+        port: env.PORT,
     }).then( () => {
-        console.log(`server taokey, na porta 3333` )
+        console.log(`server taokey, na porta ${env.PORT}` )
     })
