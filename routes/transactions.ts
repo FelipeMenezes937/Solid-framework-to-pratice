b084c1282888f25688a9e45f7dd14f2ee5303662
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2025-12-30 11:39:11 -0300

    rota get operando

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -2,10 +2,25 @@ import type { FastifyInstance } from "fastify";
 import { randomUUID } from 'node:crypto'
 import { db } from "../src/database.js"
 import { z } from "zod";
+import { request } from "node:https";
 let knex = db;
 
 // todo plugin do fastify necessariamente é uma funcao assíncrona
 export function transactionsRoutes(app: FastifyInstance){
+    
+    app.get('/', async() =>{
+        const transactions = await knex('transactions').select()
+
+        return {// trabalha como objeto pq é mais simples de adicionar/remover dados
+            total: 200,
+            transactions,
+        }
+    })
+
+    app.get('/:id', async () => {
+        
+    })
+
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
             // as coisas que eu espero que tenham no objeto que receber(req.body)
@@ -18,7 +33,7 @@ export function transactionsRoutes(app: FastifyInstance){
             .insert({
                 id: randomUUID(),
                 title,
-                amount: type == 'credit' ? amount : amount * -1,
+                amount: type == 'credit' ? amount : amount * -1,// mudando tratamento conforme o tipo
             })
 
         
