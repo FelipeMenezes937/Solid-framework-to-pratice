21548e87acc3b6811fc4ccc21e75f64ecec05e00
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2026-01-01 18:22:32 -0300

    feat: all tests e2e OK

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -64,6 +64,70 @@ describe('Transactions routes', () => {
         ])
     })
 
+    // o teste tem que se adaptar ao código
+    it('should be able to get a specific transaction', async () => {
+        const createTransaction = await request(app.server)
+        .post('/transactions')
+        .send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit', 
+        })
+        
+        const cookies = createTransaction.get('Set-Cookie')
     
+        // pedi a lista toda e usei o id da posicao [0]
+        const listTransactionsResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+        // extraindo o id da requisicao de cima
+        const transactionId = listTransactionsResponse.body.transactions[0].id
+
+        const getTransactionReponse = await request(app.server)
+        .get(`/transactions/${transactionId}`)
+        .set('Cookie', cookies)
+        .expect(200)
+        
+        expect(getTransactionReponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'New Transaction',
+                amount: 5000,
+            })
+        )
+    })
+
+        it('should be able to get the sumary', async () => {
+        const createTransaction = await request(app.server)
+        .post('/transactions')
+        .send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit', 
+        })
+        
+        const cookies = createTransaction.get('Set-Cookie')
+    
+       await request(app.server)
+        .post('/transactions')
+        .set('Cookie', cookies)
+        .send({
+                title: 'Debit transaction',
+                amount: 2000,
+                type: 'debit', 
+        })
+
+        // pedi a lista toda e usei o id da posicao [0]
+        const summaryResponse = await request(app.server)
+        .get('/transactions/summary')
+        .set('Cookie', cookies)
+        .expect(200)
+
+        
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 3000
+        })
+    })
     
 })
