c6aed94aa55de16ff32a775fae61b27140c7eeba
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2026-01-04 16:42:16 -0300

    fix bugs: knex and render integration

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,16 @@
-import { config } from "./src/database.js"
+// knexfile.ts
+import type { Knex } from "knex"
+
+const config: Knex.Config = {
+  client: "sqlite3",
+  connection: {
+    filename: "./db/app.db"
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations"
+  }
+}
 
 export default config
