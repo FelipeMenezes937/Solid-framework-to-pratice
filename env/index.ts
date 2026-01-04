e2ce7880ef2056c7307485f463816b2d04afd484
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2026-01-04 12:50:09 -0300

    chore: production config

--- env/index.ts
+++ env/index.ts
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV == 'test'){
 const envSchema = z.object({
     // ambiente em que o código tá rodando
     NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-
+    DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
     DATABASE_URL: z.string(),
-    PORT: z.number().default(3333)
+    PORT: z.coerce.number().default(3333) // coerce converte oq for recebido 
 })
 
 // pergunta se ele tá do jeito que a schema pede
--- src/database.ts
+++ src/database.ts
@@ -2,15 +2,15 @@ import { env } from '../env/index.js'
 import knex from 'knex'
 import type { Knex } from 'knex'
 
-if (!process.env.DATABASE_URL){
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL env not found")
 }
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite' ? { // o método pra sqlite != pg, tem que colocar uma verificacao
     filename: env.DATABASE_URL,
-  },
+  } : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
